9c4cf811196342f1d7caea4276f3fdc2d2783a29
nawaf83 <nawaf_83@hotmail.com>
2025-04-17 15:57:43 +0800

    second commit

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!xxx");
     }
 
     public String getGreeting() {
-        return "Hello, World!";
+        return "Hello, World!xxx";
     }
 }
