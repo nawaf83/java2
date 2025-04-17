817056685f0a7952e23c00ac5432332dfbd1ff68
nawaf83 <nawaf_83@hotmail.com>
2025-04-17 16:03:18 +0800

    3rd commit

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!xxx");
+        System.out.println("Hello, World!");
     }
 
     public String getGreeting() {
-        return "Hello, World!xxx";
+        return "Hello, World!";
     }
 }
